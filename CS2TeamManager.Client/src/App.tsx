4958f2303c57223213ec83332ba7951e8a19b88c
Adnan Jašarević <adnan.jasarevic.24@size.ba>
2026-03-02 16:06:07 +0100

    add private-route to protect authenticated pages

--- CS2TeamManager.Client/src/App.tsx
+++ CS2TeamManager.Client/src/App.tsx
@@ -2,17 +2,28 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/login';
 import Register from './pages/register';
 import Dashboard from './pages/dashboard';
+import PrivateRoute from './components/private-route'; // <-- Naša nova komponenta
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
+                {/* public routes */}
                 <Route path="/" element={<Navigate to="/login" replace />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
 
-                <Route path="/dashboard" element={<Dashboard />} />
+                {/* private routes */}
+                <Route
+                    path="/dashboard"
+                    element={
+                        <PrivateRoute>
+                            <Dashboard />
+                        </PrivateRoute>
+                    }
+                />
 
+                {/* fallbavk route */}
                 <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
         </BrowserRouter>
---
+++ CS2TeamManager.Client/src/components/private-route.tsx
@@ -0,0 +1,16 @@
+﻿import { Navigate } from 'react-router-dom';
+import React from 'react';
+
+interface PrivateRouteProps {
+    children: React.ReactNode;
+}
+
+export default function PrivateRoute({ children }: PrivateRouteProps) {
+    const token = localStorage.getItem('jwt_token');
+
+    if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return <>{children}</>;
+}
